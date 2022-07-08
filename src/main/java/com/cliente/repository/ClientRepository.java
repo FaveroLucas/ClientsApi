6f226ffb63acf3eb329778ea535ae2d94c0d4244
Lucas Fávero Souza <lucasfaverosouza@hotmail.com>
2022-07-08 14:12:21 -0300

    Update ClientRepository.java

--- src/main/java/com/cliente/repository/ClientRepository.java
+++ src/main/java/com/cliente/repository/ClientRepository.java
@@ -3,11 +3,9 @@ package com.cliente.repository;
 import com.cliente.models.Client;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ClientRepository extends MongoRepository<Client, String> {
 
     List<Client> findAllByNome(String name, Pageable page);
